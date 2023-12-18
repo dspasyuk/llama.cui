5b917116fb86a614546fa631c9621b1ae7c6357b
Denis Spasyuk <34203011+dspasyuk@users.noreply.github.com>
2023-12-17 23:15:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -71,7 +71,7 @@ config.dataChannel.set("MongoDB", {
 config.dataChannel.set("WebSearch", { datastream: "WebSearch", slice: 2000 });
 config.embedding = { MongoDB: false, Documents: true, WebSearch: false };
 config.prompt = function(userID, prompt, context){
-  return `User: '${prompt}'; ${context ? `Context: '${context}'` : ""}`;
+   return `<|im_start|> User: '${prompt}'; ${context ? `Context: '${context}'` : ""}<|im_end|>`;
 }
 
 //Piper setting
