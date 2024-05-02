ea3e00f219e8a697ab1a3e3653784af0d83f8813
Denis Spasyuk <34203011+dspasyuk@users.noreply.github.com>
2024-05-01 22:08:57 -0400

    Add files via upload

--- modeldownloader.js
+++ modeldownloader.js
@@ -39,20 +39,23 @@ async function downloadModel(modelName, outputDirectory, qs) {
     // Download the file to the specified outputDirectory
     const outputFilePath = path.join(outputDirectory, targetFile.rfilename);
     await createDirectories(outputFilePath); // Wait for directories to be created
-    const fileResponse = await axios.get(fileUrl, { responseType: "stream" });
-
-    // Create a progress bar with the total file size
-    const totalSize = parseInt(fileResponse.headers["content-length"], 10);
-    const progressBar = new ProgressBar("Downloading [:bar] :percent :etas", {
-      complete: "=",
-      incomplete: " ",
-      width: 50,
-      total: totalSize,
-    });
+    if (!fs.existsSync(outputFilePath)) {
+      const fileResponse = await axios.get(fileUrl, { responseType: "stream" });
+      // Create a progress bar with the total file size
+      const totalSize = parseInt(fileResponse.headers["content-length"], 10);
+      const progressBar = new ProgressBar("Downloading [:bar] :percent :etas", {
+        complete: "=",
+        incomplete: " ",
+        width: 50,
+        total: totalSize,
+      });
 
     // Use fs.createWriteStream to create an empty file
-    const fileWriteStream = fs.createWriteStream(outputFilePath);
-
+    
+       const fileWriteStream = fs.createWriteStream(outputFilePath);
+    } else{
+      console.log( outputFilePath, "Model file exist!");
+    }
     // Pipe the data stream to the file and update the progress bar
     fileResponse.data.on("data", (chunk) => {
       progressBar.tick(chunk.length);
